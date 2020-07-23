8bd1106806dcc041405e5fb5d7c03724b7f64ccf
Eden G <edengby@gmail.com>
2020-07-23 22:49:15 +0300

    Working server-client

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = process.env.PORT || 2221;
 
-app.listen(port, () =>
-    console.log('Express server is up and running, port '+port)
-);
+//app.use(bodyParser.json());
+//app.use(cors());
 
-app.get('/', (request, response) => {
-    res.send({ res: 'I got your request for "/" '});
+app.get('/eden', (request, response) => {
+    response.json({ res: 'I got your request for "/" '});
+    console.log('Server: server got request');
 });
+
+app.listen(port, () =>
+    console.log('Server: Express server is up and running on port '+port)
+);
--- sms-app/src/App.js
+++ sms-app/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+    const [response, setResponse] = useState('');
+
+    useEffect( () => {
+        console.log('sending request');
+        axios.get('/eden')
+            .then((response) => {
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+            if(response.status === 200) {
+                setResponse(response.data.res);
+            }
+            else {
+                setResponse('Error: '+response)
+            }
+        })
+            .catch(error => {console.log('error: ' + error)});
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {(response) ? response : 'Error' }
     </div>
   );
 }
